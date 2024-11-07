7113a1bbe5e7d72f8dcbe60fdea6817bc1c19f35
Odil-Abduhamidov <odylabduhamidov@gmail.com>
2024-11-07 21:44:30 +0500

    bags

---
+++ interfaces.ts
@@ -0,0 +1,22 @@
+interface Ishape {
+   shape: number[][]
+   color: string
+}
+interface Ishapes {
+  T: Ishape
+  S: Ishape
+  Z: Ishape
+  I: Ishape
+  O: Ishape
+  J: Ishape
+  L: Ishape
+}
+
+interface A {
+  abc: String
+}
+
+interface A {
+  xyz: number
+}
+
